982063f6e434f9793c6b83177a8918ae8bef75e7
naterayc <yndinatera10@gmail.com>
2021-12-19 14:46:47 -0300

    songs context

---
+++ src/contexts/LyricsContext.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LyricsContextProvider = () => {
+    return (
+        <div>
+            <p>Lyrics Context</p>
+        </div>
+    )
+}
+
+export default LyricsContextProvider;
